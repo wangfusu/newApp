56c6a90f432e39686bc14f453221bc41854a12cc
yckj4309 <wangwei7@cloudwalk.com>
2023-03-12 22:26:04 +0800

    up 更改结构

--- src/newApp/internal/dao/auth.go
+++
@@ -1,8 +0,0 @@
-package dao
-
-import "NewApp/internal/model"
-
-func (d *Dao) GetAuth(user, password string) (model.Auth, error) {
-	auth := model.Auth{User: user, Password: password}
-	return auth.Get(d.engine)
-}
