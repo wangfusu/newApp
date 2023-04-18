60873b905cd28a55fa5ed38d38863664db05b71b
yckj4309 <wangwei7@cloudwalk.com>
2023-04-18 17:53:09 +0800

    up

--- global/setting.go
+++ global/setting.go
@@ -6,10 +6,10 @@ import (
 )
 
 var (
-	ServerSetting   *setting.ServerSettingS
-	AppSetting      *setting.AppSettingS
-	DatabaseSetting *setting.DatabaseSettingS
-	Logger          *logger.Logger
-	JWTSetting      *setting.JWTSettingS
-	EmailSetting    *setting.EmailSettingS
+	ServerSetting   *setting.ServerSettingS   //服务设置
+	AppSetting      *setting.AppSettingS      //app设置
+	DatabaseSetting *setting.DatabaseSettingS //数据库设置
+	Logger          *logger.Logger            //日志设置
+	JWTSetting      *setting.JWTSettingS      //校验设置
+	EmailSetting    *setting.EmailSettingS    //邮件设置
 )
--- pkg/setting/section.go
+++ pkg/setting/section.go
@@ -3,50 +3,50 @@ package setting
 import "time"
 
 type ServerSettingS struct {
-	RunMode        string
-	HttpPort       string
-	ReadTimeout    time.Duration
-	WriteTimeout   time.Duration
-	ContextTimeout time.Duration
+	RunMode        string        //运行模式
+	HttpPort       string        //运行端口
+	ReadTimeout    time.Duration //读超时 时间
+	WriteTimeout   time.Duration //写超时时间
+	ContextTimeout time.Duration //上下文超时时间
 }
 type AppSettingS struct {
-	DefaultPageSize      int
-	MaxPageSize          int
-	LogSavePath          string
-	LogFileName          string
-	LogFileExt           string
+	DefaultPageSize      int      //默认分页大小
+	MaxPageSize          int      //最大分页大小
+	LogSavePath          string   //日志保存路径
+	LogFileName          string   //日志文件名称
+	LogFileExt           string   //日志文件后缀名
 	UploadSavePath       string   //上传文件的最终保存目录
 	UploadServerUrl      string   //上传文件后的用于展示的文件服务地址。
 	UploadImageMaxSize   int      //上传文件所允许的最大空间大小（MB）
 	UploadImageAllowExts []string //上传文件所允许的文件后缀。
 }
 type DatabaseSettingS struct {
-	DBType       string
-	UserName     string
-	Password     string
-	Host         string
-	DBName       string
-	TablePrefix  string
-	Charset      string
-	ParseTime    bool
-	MaxIdleConns int
-	MaxOpenConns int
+	DBType       string //数据库类型
+	UserName     string //账号
+	Password     string //密码
+	Host         string //数据库地址
+	DBName       string //数据库名
+	TablePrefix  string //
+	Charset      string //数据库文本格式
+	ParseTime    bool   //
+	MaxIdleConns int    //
+	MaxOpenConns int    //
 }
 
 type JWTSettingS struct {
-	Secret string
-	Issuer string
-	Expire time.Duration
+	Secret string        //密钥
+	Issuer string        //名称
+	Expire time.Duration //鉴权时间
 }
 
 type EmailSettingS struct {
-	Host     string
-	Port     int
-	UserName string
-	Password string
-	IsSSL    bool
-	From     string
-	To       []string
+	Host     string   //ip地址
+	Port     int      //端口
+	UserName string   //账号
+	Password string   //密码（一般指代邮箱的对接的密钥）
+	IsSSL    bool     //是否ssl
+	From     string   //from名称
+	To       []string //接收邮件用户
 }
 
 var sections = make(map[string]interface{})
